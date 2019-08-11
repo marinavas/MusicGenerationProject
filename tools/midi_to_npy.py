53d6fedcf0102109b8d85cb4f4bea54623021e1c
vasiljevic98 <vasiljevic98@hotmail.rs>
2019-08-11 10:31:31 +0200

    npy_to_midi

--- tools/midi_to_npy.py
+++ tools/midi_to_npy.py
@@ -23,13 +23,12 @@ for file in os.listdir('data\Data_abc\Midi'):
         piano_midi = midi_pretty_format.instruments[0] # Get the piano channels
         piano_roll = piano_midi.get_piano_roll(fs = 4)
 
-        #print(np.max(piano_roll))
-
-
+        
         piano = np.zeros((128,piano_roll.shape[1]+16))
+        np.save("data/results/res1.npy",piano_roll)
         piano[:,16:] = piano_roll
 
-        #matplotlib.pyplot.imsave("data/data128/"+str(i)+".png",piano,cmap = "gray")
+       
         i+=1
         
         if(i%10 == 0):
@@ -40,6 +39,7 @@ for file in os.listdir('data\Data_abc\Midi'):
                 #print((piano[:,w+16:w+32].T).shape)
                 data_x.append(piano[:,w+16:w+32].T)
                 data_prev.append(piano[:,w:w+16].T)
+        
 
 data_x =np.array(np.reshape(data_x,(-1,16,128,1)))
 data_prev =np.array(np.reshape(data_prev,(-1,16,128,1)))
---
+++ tools/npy_to_midi.py
@@ -0,0 +1,53 @@
+import numpy as np
+import matplotlib
+import os
+from matplotlib import pyplot as plt
+import pretty_midi
+
+def piano_roll_to_pretty_midi(piano_roll, fs=100, program=0):
+  
+    notes, frames = piano_roll.shape
+    pm = pretty_midi.PrettyMIDI()
+    instrument = pretty_midi.Instrument(program=program)
+
+    # pad 1 column of zeros so we can acknowledge inital and ending events
+    piano_roll = np.pad(piano_roll, [(0, 0), (1, 1)], 'constant')
+
+    # use changes in velocities to find note on / note off events
+    velocity_changes = np.nonzero(np.diff(piano_roll).T)
+
+    # keep track on velocities and note on times
+    prev_velocities = np.zeros(notes, dtype=int)
+    note_on_time = np.zeros(notes)
+
+    for time, note in zip(*velocity_changes):
+        # use time + 1 because of padding above
+        velocity = piano_roll[note, time + 1]
+        time = time / fs
+        if velocity > 0:
+            if prev_velocities[note] == 0:
+                note_on_time[note] = time
+                prev_velocities[note] = velocity
+        else:
+            pm_note = pretty_midi.Note(
+                velocity=prev_velocities[note],
+                pitch=note,
+                start=note_on_time[note],
+                end=time)
+            instrument.notes.append(pm_note)
+            prev_velocities[note] = 0
+    pm.instruments.append(instrument)
+    return pm
+
+
+for file in os.listdir('data\\results'):
+    if file.endswith(".npy"):
+        path =os.path.join('data\\results', file)
+
+        img = np.load(path)
+        midi = piano_roll_to_pretty_midi(img,4)
+        midi.write("djole.mid")
+
+       
+
+
